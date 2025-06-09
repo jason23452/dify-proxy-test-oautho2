8274528441cbc869230bba3e9c6492493f7366e3
jason23452 <andt30610@gmail.com>
2025-06-09 17:14:56 +0800

    0.0.2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      // 這裡把 '@' 映射到 src 資料夾
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
