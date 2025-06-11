751b02ff56301d06e0c7fd42c409dcb013596775
jason23452 <andt30610@gmail.com>
2025-06-11 16:00:32 +0800

    新增taiwind css

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,31 @@
 // vite.config.js
-import { defineConfig, loadEnv } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { fileURLToPath, URL } from "node:url";
+import vueDevTools from "vite-plugin-vue-devtools";
+import svgLoader from "vite-svg-loader";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE_')
+  const env = loadEnv(mode, process.cwd(), "VITE_");
   return defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), vueDevTools(), svgLoader(), tailwindcss()],
     resolve: {
-      alias: { '@': path.resolve(__dirname, 'src') }
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
     server: {
-      port:"3000",
+      port: "3000",
       proxy: {
-        '/dify': {
+        "/dify": {
           target: env.VITE_DIFY_BASE_URL, // e.g. https://api.dify.ai/v1
           changeOrigin: true,
           // 把 /dify 開頭全都去掉，保留後面路徑
-          rewrite: path => path.replace(/^\/dify/, '')
-        }
-      }
-    }
-  })
-}
+          rewrite: (path) => path.replace(/^\/dify/, ""),
+        },
+      },
+    },
+  });
+};
